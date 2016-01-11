852bff0682c3ba3b26c72e54917d59c93c276744
alexsclim <alexsclim@gmail.com>
2016-01-10 20:18:28 -0800

    add templates

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Budgeteer"
   end
 
-  test "should get help" do
-    get :help
+  test "should get settings" do
+    get :settings
     assert_response :success
+    assert_select "title", "Settings | Budgeteer"
   end
 
+  test "should get budget" do
+    get :budget
+    assert_response :success
+    assert_select "title", "Budgets | Budgeteer"
+  end
 end
