b3953e837c9326dde8dea4e2af657c9a1db0d415
alexsclim <alexsclim@gmail.com>
2016-01-15 16:01:32 -0800

    fix gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root              'static_pages#home'
   get 'settings' => 'static_pages#settings'
   get 'budgets'  => 'static_pages#budgets'
   get 'contact'  => 'static_pages#contact'
+  get 'signup'   => 'users#new'
 end
