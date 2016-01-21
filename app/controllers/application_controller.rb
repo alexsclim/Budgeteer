6712047ea5428075dd031e20d2c9957961f48eb6
Alex Lim <alexsclim@gmail.com>
2016-01-21 00:57:04 -0800

    add user login parts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  #Logs in given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root              'static_pages#home'
-  get 'settings' => 'static_pages#settings'
-  get 'budgets'  => 'static_pages#budgets'
-  get 'contact'  => 'static_pages#contact'
-  get 'signup'   => 'users#new'
+  get   'settings' => 'static_pages#settings'
+  get   'budgets'  => 'static_pages#budgets'
+  get   'contact'  => 'static_pages#contact'
+  get   'signup'   => 'users#new'
+  get   'login'    => 'sessions#new'
+  post  'login'    => 'sessions#create'
+  delete'logout'   => 'sessions#destroy'
   resources :users
 end
