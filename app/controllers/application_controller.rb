d9fa9fda484362505c88bb240d5ab45e57130609
alexsclim <alexsclim@gmail.com>
2016-01-25 09:24:49 -0800

    logout remember

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  #Logs in given user
-  def log_in(user)
-    session[:user_id] = user.id
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  attr_accessor :remember_token
 
   #Returns the hash digest of the given string
   def User.digest(string)
@@ -18,5 +19,16 @@ class User < ActiveRecord::Base
   #Returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  #Remembers a user in the database for use in persistant sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
