065ff1e249c6850e347d248feac799a154d7cb4a
alexsclim <alexsclim@gmail.com>
2016-01-31 22:54:05 -0800

    last nchanges

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root              'static_pages#home'
-  get   'settings' => 'static_pages#settings'
-  get   'budgets'  => 'static_pages#budgets'
-  get   'contact'  => 'static_pages#contact'
-  get   'signup'   => 'users#new'
-  get   'login'    => 'sessions#new'
-  post  'login'    => 'sessions#create'
-  delete'logout'   => 'sessions#destroy'
+  root                  'static_pages#home'
+  get   'settings', to:  'static_pages#settings'
+  get   'budgets',  to:  'static_pages#budgets'
+  get   'contact',  to:  'static_pages#contact'
+  get   'signup',   to:  'users#new'
+  get   'login',    to:  'sessions#new'
+  post  'login',    to:  'sessions#create'
+  delete'logout',   to:  'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
