6d52b938a4e0fa0c85ce83b58a4a1f7aff658b61
alexsclim <alexsclim@gmail.com>
2016-02-08 23:00:03 -0800

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202084724) do
+ActiveRecord::Schema.define(version: 20160209055908) do
 
   create_table "budgets", force: :cascade do |t|
     t.date     "year"
