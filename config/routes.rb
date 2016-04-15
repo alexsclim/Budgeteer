5ea1c935c560f4450d7f2003e7cb25451131b691
Alex Lim <alexsclim@gmail.com>
2016-04-14 22:06:36 -0700

    add budgets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'budgets#index', as: :authenticated_root
+  end
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :budgets
 end
